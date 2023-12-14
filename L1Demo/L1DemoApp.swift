aa9007b7f1c6b98892b2cc9944afcc208b375fcb
feroz-hub <bashaferoz66@gmail.com>
2023-12-14 22:58:46 +0530

    Initial Commit

---
+++ L1Demo/L1DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1DemoApp.swift
+//  L1Demo
+//
+//  Created by Feroze Basha on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct L1DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
